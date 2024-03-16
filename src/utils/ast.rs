b46e81f3c69cf81be7e5f8533891dc53d8ac121e
DevMoozi <devmoozi@gmail.com>
2024-03-16 21:33:26 +0900

    is_comment_exist 유틸 추가

--- src/utils/ast.rs
+++ src/utils/ast.rs
@@ -1,1 +1,8 @@
-// pub fn find_comment() -> Box<str> {}
+use swc_core::common::{
+    comments::Comment,
+    pass::{AstNodePath, NodeRef},
+};
+
+pub fn is_comment_exist(comments: &Vec<Comment>, comment: &str) -> bool {
+    return comments.iter().any(|&x| x.text.trim() == comment);
+}
