31c7124e2441ad2e95339af2f565d951565d7c31
CreeJee <creejee@quotabook.com>
2024-03-17 21:16:24 +0900

    feat: is_jsx_component_element 함수 추가

--- src/utils/get_import_specifier.rs
+++
@@ -1,5 +0,0 @@
-use swc_core::ecma::ast::Program;
-
-use crate::config::ProgramStateContext;
-
-pub fn get_import_specifier(state: ProgramStateContext, program: Program) {}
--- src/utils/is_jsx_component_element.rs
+++ src/utils/is_jsx_component_element.rs
@@ -1,25 +1,31 @@
-use swc_core::ecma::ast::{JSXAttr, JSXMemberExpr};
+use swc_core::ecma::ast::{JSXAttrOrSpread, JSXElement, JSXElementName};
 
 use regex::Regex;
 
 use super::jsx::{is_attribute_name_ref, is_attribute_unsupported};
 
-pub enum JSXComponentable {
-    JSXMemberExpr(JSXMemberExpr),
-    JSXAttr(JSXAttr),
-}
-pub fn is_jsx_component_element(expr: JSXComponentable) -> bool {
+pub fn is_jsx_component_element(jsx: JSXElement) -> bool {
     let component_name_regex = Regex::new("^[A-Z_]").unwrap();
-    match expr {
-        JSXComponentable::JSXAttr(attr) => {
-            let is_unsupported = is_attribute_unsupported(attr.clone());
-            let is_attribute_ref = is_attribute_name_ref(attr.clone());
-            return is_unsupported || is_attribute_ref;
-        }
-        JSXComponentable::JSXMemberExpr(expr) => {
-            let content = expr.prop.to_string();
+
+    let is_member_expr_is_acceptable = match jsx.opening.name {
+        JSXElementName::Ident(ident) => {
+            let content = ident.sym.to_string();
             //@TODO : 여기서 재귀로 JSXComponentable::JSXAttr을 분기 태우는게 원래의 로직해석에 맞는데 그 방법을 정확히 모르겟음.
             return component_name_regex.is_match(content.as_str());
         }
-    }
+        JSXElementName::JSXMemberExpr(expr) => true,
+        _ => false,
+    };
+    let is_attr_has_whitelist = jsx.opening.attrs.iter().any(|attr| match attr {
+        JSXAttrOrSpread::JSXAttr(attr) => {
+            let is_unsupported = is_attribute_unsupported(attr);
+            let is_attribute_ref = is_attribute_name_ref(attr);
+            return is_unsupported || is_attribute_ref;
+        }
+
+        //@TODO: 스팩상 spread element에 대한 처리가 없는데 정말 불가능한가?
+        JSXAttrOrSpread::SpreadElement(SpreadElement) => false,
+    });
+
+    return is_member_expr_is_acceptable || is_attr_has_whitelist;
 }
