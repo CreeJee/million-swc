07092856fd900204b78491d913fd2d3b5b87d7ab
CreeJee <creejee@quotabook.com>
2024-03-17 15:43:16 +0900

    feat: is_jsx_component_element 구현

--- src/utils/is_jsx_component_element.rs
+++ src/utils/is_jsx_component_element.rs
@@ -2,6 +2,8 @@ use swc_core::ecma::ast::{JSXAttr, JSXMemberExpr};
 
 use regex::Regex;
 
+use super::jsx::{is_attribute_name_ref, is_attribute_unsupported};
+
 pub enum JSXComponentable {
     JSXMemberExpr(JSXMemberExpr),
     JSXAttr(JSXAttr),
@@ -9,9 +11,14 @@ pub enum JSXComponentable {
 pub fn is_jsx_component_element(expr: JSXComponentable) -> bool {
     let component_name_regex = Regex::new("^[A-Z_]").unwrap();
     match expr {
-        JSXComponentable::JSXAttr(JSXAttr { span, name, value }) => false,
-        JSXComponentable::JSXMemberExpr(JSXMemberExpr { obj, prop }) => {
-            let content = prop.to_string();
+        JSXComponentable::JSXAttr(attr) => {
+            let is_unsupported = is_attribute_unsupported(attr.clone());
+            let is_attribute_ref = is_attribute_name_ref(attr.clone());
+            return is_unsupported || is_attribute_ref;
+        }
+        JSXComponentable::JSXMemberExpr(expr) => {
+            let content = expr.prop.to_string();
+            //@TODO : 여기서 재귀로 JSXComponentable::JSXAttr을 분기 태우는게 원래의 로직해석에 맞는데 그 방법을 정확히 모르겟음.
             return component_name_regex.is_match(content.as_str());
         }
     }
--- src/utils/jsx.rs
+++ src/utils/jsx.rs
@@ -1,15 +1,18 @@
 use swc_core::ecma::ast::{JSXAttr, JSXAttrName};
 
 pub fn is_attribute_unsupported(attribute: JSXAttr) -> bool {
-    let unsupported = vec!["tw", "css", "ref"];
+    let unsupported = vec!["tw", "css"];
     return match attribute.name {
-        JSXAttrName::JSXNamespacedName(namespace) => {
-            let sym = namespace.name.sym.to_string();
-            unsupported.contains(&sym.as_str())
-        }
         JSXAttrName::Ident(ident) => {
             let sym = ident.sym.to_string();
             unsupported.contains(&sym.as_str())
         }
+        _ => false,
+    };
+}
+pub fn is_attribute_name_ref(attribute: JSXAttr) -> bool {
+    return match attribute.name {
+        JSXAttrName::Ident(ident) => ident.sym.to_string().eq("ref"),
+        _ => false,
     };
 }
