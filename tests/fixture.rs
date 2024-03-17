a2f5566d493ddf525cbd260db7d7b5e5691b2125
CreeJee <creejee@quotabook.com>
2024-03-17 09:24:43 +0900

    chore: unused-import 정리

--- tests/fixture.rs
+++ tests/fixture.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, fs, path::PathBuf};
 
 use million_swc::{
     config::{self, Config, ProgramStateContext, ServerMode},
-    transform::{auto_transform::HOCTrasnformVisitor, block_transform::million_block_transform},
+    transform::block_transform::million_block_transform,
 };
 use swc_core::{
     common::{chain, Mark},
@@ -10,9 +10,8 @@ use swc_core::{
         parser::{EsConfig, Syntax},
         transforms::{
             base::resolver,
-            testing::{test, test_fixture, test_transform, FixtureTestConfig},
+            testing::{test, test_fixture, FixtureTestConfig},
         },
-        visit::as_folder,
     },
 };
 fn syntax() -> Syntax {
@@ -24,19 +23,6 @@ fn syntax() -> Syntax {
 
 #[testing::fixture("tests/fixture/**/input.js")]
 fn fixture(input: PathBuf) {
-    let ref mut context: config::ProgramStateContext = ProgramStateContext {
-        options: Config {
-            hmr: Some(false),
-            server: Some(false),
-            auto: Some(false),
-            log: true,
-            rsc: false,
-        },
-        identifiers: HashMap::new(),
-        namespaces: HashMap::new(),
-        imports: HashMap::new(),
-        server_mode: ServerMode::Client,
-    };
     let output = input.parent().unwrap().join("output.js");
     test_fixture(
         syntax(),
@@ -45,7 +31,13 @@ fn fixture(input: PathBuf) {
             let top_level_mark = Mark::new();
             chain!(
                 resolver(unresolved_mark, top_level_mark, false),
-                million_block_transform(context)
+                million_block_transform(ProgramStateContext::from(Config {
+                    hmr: Some(false),
+                    server: Some(false),
+                    auto: Some(false),
+                    log: true,
+                    rsc: false,
+                }))
             )
         },
         &input,
